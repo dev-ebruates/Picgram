4030dd1e19a9894793dfd94c92665ca1a5b69d1e
dev-ebruates <devatesebru@gmail.com>
2025-01-01 23:32:28 +0300

    Feed useEffect kaldırıldı

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,17 +1,13 @@
 import Post from "../post/Post";
 import { useGetAllPostsQuery } from "../../features/postFeatures/postApi";
-import { useEffect } from "react";
 import "./Feed.css";
 
 const Feed = () => {  
   const { data: posts, isLoading: isLoading, error: error } = useGetAllPostsQuery();
 
-  useEffect(() => {
-  }, []);
-
   if (isLoading) return <div>Yükleniyor...</div>;
   if (error) return <div>Hata: {error}</div>;
-
+  
   return (
     <div className="feed">
       {posts?.map((post, i) => (
