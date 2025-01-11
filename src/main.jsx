f7156cbaa72e4f30313dd2f8caec7b7f495a6e4a
dev-ebruates <devatesebru@gmail.com>
2025-01-11 17:22:27 +0300

    Google giriş butonu eklendi

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const clientId = "1050319585875-1ujql7a4palt2csnbm2ohf1ufbcr1mio.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  <GoogleOAuthProvider clientId={clientId}>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </GoogleOAuthProvider>
 );
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,6 +4,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuthMutation } from "../features/authFeatures/authApi.js";
 import { useDispatch } from "react-redux";
 import { setCredentials } from "../features/authFeatures/authSlice.js";
+import { GoogleLogin } from "@react-oauth/google";
+import {jwtDecode} from "jwt-decode";
 
 const LoginPage = () => {
   const [formData, setFormData] = useState({
@@ -12,13 +14,11 @@ const LoginPage = () => {
   });
 
   const dispatch = useDispatch();
-  const [authMutation, {
-    error: authError,
-    isLoading: authLoading,
-  }] = useAuthMutation();
+  const [authMutation, { error: authError, isLoading: authLoading }] =
+    useAuthMutation();
 
   const navigate = useNavigate();
-  
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prev) => ({
@@ -35,7 +35,7 @@ const LoginPage = () => {
         const userData = {
           username: result.data.data.username,
           token: result.data.data.token,
-          role: result.data.data.role
+          role: result.data.data.role,
         };
         dispatch(setCredentials(userData));
         navigate("/");
@@ -94,43 +94,25 @@ const LoginPage = () => {
             <hr className="w-1/4 border-gray-300" />
           </div>
           <div className="flex justify-center space-x-4">
-            {/* Facebook İkonu */}
-            <button className="flex items-center justify-center w-10 h-10 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="h-6 w-6"
-              >
-                <path d="M22.675 0H1.325C.594 0 0 .594 0 1.326v21.348C0 23.406.594 24 1.325 24h11.495v-9.294H9.69V11.41h3.13V8.822c0-3.1 1.893-4.787 4.657-4.787 1.325 0 2.463.099 2.794.143v3.24h-1.918c-1.506 0-1.798.716-1.798 1.765v2.314h3.595l-.468 3.297h-3.127V24h6.127c.73 0 1.324-.594 1.324-1.326V1.326C24 .594 23.406 0 22.675 0z" />
-              </svg>
-            </button>
-
-            {/* Google İkonu */}
-            <button className="flex items-center justify-center w-10 h-10 bg-gray-100 text-gray-700 rounded-full border border-gray-300 hover:bg-gray-200 transition">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 48 48"
-                className="h-6 w-6"
-              >
-                <path
-                  fill="#EA4335"
-                  d="M24 9.5c3.54 0 6.36 1.44 8.31 3.69l6.19-6.19C34.18 3.56 29.54 1.5 24 1.5 14.86 1.5 7.16 7.16 4.23 15.04l7.89 6.15C13.65 14.53 18.48 9.5 24 9.5z"
-                />
-                <path
-                  fill="#4285F4"
-                  d="M46.5 24c0-1.74-.2-3.43-.57-5.05H24v9.57h12.59c-.54 2.86-2.15 5.28-4.56 6.91l7.29 5.68c4.25-3.92 6.68-9.7 6.68-16.11z"
-                />
-                <path
-                  fill="#FBBC05"
-                  d="M4.23 32.96c-1.22-2.46-1.93-5.2-1.93-8.03s.71-5.57 1.93-8.03L-3.66 10.23C-5.52 14.14-6.5 18.93-6.5 24s.98 9.86 2.84 13.77l7.89-6.15z"
-                />
-                <path
-                  fill="#34A853"
-                  d="M24 46.5c5.87 0 10.79-1.96 14.37-5.3l-7.29-5.68c-2.01 1.33-4.6 2.12-7.08 2.12-5.52 0-10.35-5.03-12.08-11.69l-7.89 6.15C7.16 40.84 14.86 46.5 24 46.5z"
-                />
-              </svg>
-            </button>
+            <span>
+              <GoogleLogin
+                onSuccess={(credentialResponse) => {
+                  console.log(credentialResponse);
+                  const decoded = jwtDecode(credentialResponse.credential);
+                  console.log(decoded);
+                  // const userData = {
+                  //   username: decoded.name,
+                  //   token: credentialResponse.credential,
+                  //   role: "user",
+                  // };
+                  // dispatch(setCredentials(userData));
+                  // navigate("/");
+                }}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
+              />
+            </span>
           </div>
 
           <div className="text-center text-sm text-gray-500 mt-4">
