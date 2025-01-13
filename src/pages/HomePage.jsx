456435780496473b7893dfed330eae1529ceb6a7
dev-ebruates <devatesebru@gmail.com>
2025-01-13 23:06:08 +0300

    .

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import Feed from "../components/Feed/Feed";
 import Header from "../components/Header/Header";
-import Story from "../components/story/Story";
+import Story from "../components/Story/Story.jsx";
 
 
 const HomePage = () => {
