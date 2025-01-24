356f89bbd4ed2f1b0416b118acb76a8e8c2d83fb
dev-ebruates <devatesebru@gmail.com>
2025-01-24 19:12:39 +0300

    modal refactoring

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,13 +1,24 @@
+import { useEffect } from "react";
+
 const Modal = ({ isOpen, onClose, children }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => (document.body.style.overflow = ""); // Cleanup
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return (
     <div
-      className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-50"
+      className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-[100]"
       onClick={onClose}
     >
       <div
-        className=" bg-gray-900 p-6 rounded-lg shadow-lg relative w-full max-w-md border-gray-700 border-2 "
+        className="bg-gray-900 p-6 rounded-lg shadow-lg relative w-full max-w-md border-gray-700 border-2"
         onClick={(e) => e.stopPropagation()}
       >
         <button
