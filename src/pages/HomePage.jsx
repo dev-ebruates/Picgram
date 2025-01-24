f891921561a4b4cacb224da6655a630c35600065
dev-ebruates <devatesebru@gmail.com>
2025-01-24 14:14:46 +0300

    refactoring

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,14 @@ import Story from "../components/Story/Story.jsx";
 
 const HomePage = () => {
   return (
-    <div className="flex flex-row justify-between ">
-        <div className="flex flex-col items-center ">
+    <div className="flex justify-start w-full min-h-screen bg-black pt-[70px] pl-[10vw]">
+      <div className="w-[600px] max-w-full flex flex-col items-center">
+        <div className="-mt-10 p-4">
           <Story />
-          <Feed />
         </div>
+        <Feed />
       </div>
+    </div>
   );
 };
 
