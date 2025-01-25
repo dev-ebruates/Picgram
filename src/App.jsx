ff1fab07222fda487885ccfb07c1603ab8aa83ac
dev-ebruates <devatesebru@gmail.com>
2025-01-25 15:10:43 +0300

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import HomePage from "./pages/HomePage";
 import { Routes, Route, Navigate } from "react-router-dom";
 import ProfilePage from "./pages/ProfilePage";
-import StoryPage from "./pages/StoryPage";
 import MessagePage from "./pages/MessagePage";
 import CommentsPage from "./pages/CommentsPage.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/:username" element={<ProfilePage />} />
+        <Route path="/" element={<ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>} />
+        <Route path="/:username" element={<ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>} />
         
-        <Route path="/messages" element={<MessagePage />} />
-        <Route path="/comments" element={<CommentsPage />} />
+        <Route path="/messages" element={<ProtectedRoute>
+                <MessagePage />
+              </ProtectedRoute>} />
+        <Route path="/comments" element={<ProtectedRoute>
+                <CommentsPage />
+              </ProtectedRoute>} />
 
         {/* Bilinmeyen URL'leri ana sayfaya yönlendir */}
         <Route path="*" element={<Navigate to="/login" replace />} />
