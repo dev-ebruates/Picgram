02319339497f08d365931bae461ebc50b9b60a0a
dev-ebruates <devatesebru@gmail.com>
2025-01-30 17:40:49 +0300

    refactoring

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,33 @@
+import React, { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectIsAuthenticated } from '../features/authFeatures/authSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectIsAuthenticated, logout } from '../features/authFeatures/authSlice';
+import { jwtDecode } from 'jwt-decode';
 
 const ProtectedRoute = ({ children, requiredRole }) => {
+  const dispatch = useDispatch();
   const isAuthenticated = useSelector(selectIsAuthenticated);
+  const token = localStorage.getItem('authToken');
   const userRole = parseInt(localStorage.getItem("role"), 10);
+
+  useEffect(() => {
+    // Token kontrolü
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        const currentTime = Date.now() / 1000;
+
+        // Token süresi dolmuş veya geçersiz ise
+        if (decodedToken.exp < currentTime) {
+          dispatch(logout());
+          return;
+        }
+      } catch (error) {
+        // Token decode edilemiyorsa (bozuk token)
+        dispatch(logout());
+      }
+    }
+  }, [token, dispatch]);
 
   if (!isAuthenticated) {
     // Kullanıcı giriş yapmamışsa login sayfasına yönlendir
